01472ced344996128d9dff4cd2fa9346dc36855f
inays <sykaninay@gmail.com>
2025-10-27 21:10:45 +0700

    seeder

--- database/seeders/MahasiswaSeeder.php
+++ database/seeders/MahasiswaSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; // <--- Tambahkan ini
 
 class MahasiswaSeeder extends Seeder
 {
@@ -13,12 +13,11 @@ class MahasiswaSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('mahasiswas')->insert([
-            'nama' => 'Kimberly',
-            'nim' => '132424008',
-            'prodi' => 'Teknologi Informasi',
-            'email' => 'kimberlyez@gmail.com',
-            'nohp' => '62786552353'
-        ]);
-    }
-}
+        DB::table('mahasiswas')-insert([
+            'nama' =>"Kimberly",
+            'nim' =>"132424008",
+            'prodi' =>"Teknologi Informasi",
+            'email' =>"kimberlyez@gmail.com",
+            'nohp' =>62786552353
+        ])
+        }
